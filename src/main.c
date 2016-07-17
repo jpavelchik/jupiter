69005ada514b001ee939a2134f6483f350e371e3
John Pavelchik <john@smartpointer.net>
2016-07-16 19:41:31 -0700

    Added thread to jupiter and associated autoconf changes to support it

--- src/main.c
+++ src/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#if HAVE_CONIFG_H
+#	include <config.h>
+#endif
+
+#if HAVE_PTHREAD_H
+#	include <pthread.h>
+#endif
+
+
+static void* print_it(void* data)
+{
+	printf("Hello from %s!\n", (const char*)data);
+	return (void*)0;
+}
+
+
 int main(int argc, char* argv[])
 {
-	printf("Hello from %s!\n", argv[0]);
+#if ASYNC_EXEC
+	pthread_t tid;
+	pthread_create(&tid, 0, print_it, argv[0]);
+	pthread_join(tid, 0);
+#else
+	print_it(argv[0]);
+#endif
+
 	return EXIT_SUCCESS;
 }
